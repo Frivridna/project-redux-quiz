ce616c89b458ff406dc5735e9a10343184d2bac5
M4L5 <malin.gu2017@gmail.com>
2021-04-13 11:20:24 +0200

    save

---
+++ src/components/Images.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { lime } from '../assets/lime'
+
+export const Image = () => {
+    return(
+        <img 
+        src={lime}
+        alt="picture of lime"
+        >
+        </img>
+    )
+}
